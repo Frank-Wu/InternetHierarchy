1306631079ba4fd474c7a3132a40b039d07d02ff
yingjun <yingjun@comp.nus.edu.sg>
2014-06-29 01:43:03 +0800

    update

--- scripts/generate_bview_path.sh
+++ scripts/generate_bview_path.sh
@@ -1,4 +1,6 @@
-year=2011
-for month in {01..12};
-	do find data.ris.ripe.net/rrc${1}/${year}.${month}/ -name bview.${year}${month}0[1-5].* |xargs -I{} ./bgpdump {}|grep ASPATH >> rawdataset/rrc${1}.${year}.${month}&
+for year in {2004..2013};
+do
+for monthalg in {1..12};
+	do month=`printf "%02d" $monthalg`;find data.ris.ripe.net/rrc${1}/${year}.${month}/ -name bview.${year}${month}0[1-5].* |xargs -I{} ./bgpdump {}|grep ASPATH >> rawdataset/rrc${1}.${year}.${month}&
 	done;
+done;
--- scripts/run_hmm.sh
+++ scripts/run_hmm.sh
@@ -1,8 +1,11 @@
-for year in {2002..2010};
+year=2011
+for month in {01..06};
 do 
-	obs_num=`cat hmmdataset/$year.01.seq.size`;
-	for state_num in {4..7};
-	do 
-		./jahmm.sh learn-bw -opdf integer -r $obs_num -is hmmdataset/$year.01.seq -ni 1000 -i hmmdataset/$year.01.$state_num.hmm -o hmmresult/$year.01.$state_num.hmm &
-	done;
+	obs_num=`cat hmmdataset/${year}.${month}.seq.size`;
+	state_num=6
+#	sleep 20m
+	#for state_num in {4..7};
+#	do 
+		./jahmm.sh learn-bw -opdf integer -r $obs_num -is hmmdataset/${year}.${month}.seq -ni 1000 -i hmmdataset/${year}.${month}.${state_num}.hmm -o hmmresult/${year}.${month}.${state_num}.hmm &
+#	done;
 done;
